599d8a05da15cea168491794a7155a781f47b880
Matt Rasmussen <Matthew@Matts-Mac-mini.local>
2015-12-05 16:18:36 -0700

    i don't know what I'm doing

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--WebSiteHome-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Matt's First Web Server</title>
+    </head>
+    <body>
+        <h2 align='center'>Please select a course below</h2>
+        <p><a href="/WebDev/first.html">CSIS 2440</a></p>
+        <p><a href="/WebProg/index.php">CSIS 2470</a></p>
+        <p><a href="mediaTest.php">Styles Test</a></p>
+        <p><a href="surveyResults.php">Survey Results</a></p>
+        <?php
+            echo 'Computer programming is cool';
+        ?>
+    </body>
+</html>
